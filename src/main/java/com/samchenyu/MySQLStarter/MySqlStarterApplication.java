81100769aef8590bf90895de8559f07693e3fcb1
Sam Chen Yu <150127006+SamChenYu@users.noreply.github.com>
2024-08-06 12:33:49 +0800

    Initial Commit

---
+++ src/main/java/com/samchenyu/MySQLStarter/MySqlStarterApplication.java
@@ -0,0 +1,13 @@
+package com.samchenyu.MySQLStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqlStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samchenyu/MySQLStarter/MySqlStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samchenyu.MySQLStarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqlStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
